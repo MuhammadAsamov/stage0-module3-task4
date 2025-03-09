5a1a4db84a953bb5096fa057d78e8f346aa43fd5
muhammad <mailasmu@gmail.com>
2025-03-09 06:21:42 +0500

    done

--- src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
+++ src/main/java/school/mjc/stage0/module3/task4/IntRepresentation.java
@@ -2,5 +2,14 @@ package school.mjc.stage0.module3.task4;
 
 public class IntRepresentation {
     public static void main(String[] args) {
+        int c = 99;  // ASCII value of 'c'
+        int h = 104; // ASCII value of 'h'
+        int a = 97;  // ASCII value of 'a'
+        int r = 114; // ASCII value of 'r'
+
+        System.out.println((char) c);
+        System.out.println((char) h);
+        System.out.println((char) a);
+        System.out.println((char) r);
     }
 }
